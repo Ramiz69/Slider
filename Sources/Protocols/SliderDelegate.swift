820e264809205e97af2e284b0f1c35d03fec229c
Ramiz Kichibekov <angelokk161@gmail.com>
2024-03-17 10:01:30 +0400

    Minor bug fix;

--- Sources/Protocols/SliderDelegate.swift
+++ Sources/Protocols/SliderDelegate.swift
@@ -21,7 +21,9 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 //
+
 import Foundation
+import CoreFoundation
 
 @MainActor
 public protocol SliderDelegate: AnyObject {

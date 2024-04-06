9acf546bc334133021abecd6a00c49d419df3a5c
Kichibekov Ramiz <Ramiz161@icloud.com>
2024-04-06 23:38:23 +0400

    no message

--- Package.swift
+++ Package.swift
@@ -12,9 +12,7 @@ let package = Package(
             name: "Slider",
             targets: ["Slider"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Slider",
